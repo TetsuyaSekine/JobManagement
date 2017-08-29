4a6b6905a90d70d40a0dcb888f749afc99b9a2ad
tetsuya.sekine <gfbf5_tetsu@yahoo.co.jp>
2017-08-29 22:19:14 +0900

    カレンダー表示を修正

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,8 +1,22 @@
 $(document).ready(function(){
   $('.datepicker').datepicker({
-      changeMonth: true,
-      changeYear: true,
-      dateFormat: 'yy/mm/dd',
-      yearRange: '2010:2030'
+    language: 'ja',
+    beforeShowDay: function(date) {
+      var myDate = new Object();
+      if (date.getDay() == 0) {
+        myDate.enabled = true;
+        myDate.classes = 'class-sunday';
+        myDate.tooltip  = '日曜日';
+      } else if　(date.getDay() == 6) {
+        myDate.enabled = true;
+        myDate.classes = 'class-saturday';
+        myDate.tooltip  = '土曜日';
+      } else {
+        myDate.enabled = true;
+        myDate.classes = 'class-weekday';
+        myDate.tooltip  = '平日';
+      }
+      return myDate;
+    }
   });
 });
