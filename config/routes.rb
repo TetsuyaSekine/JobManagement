4dcb95af75105e35c9aa0e63490edd02a84a7c52
root <tetsuya.sekine@persol.co.jp>
2017-09-01 13:11:16 +0000

    create new object tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :tasks
   resources :shains
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/ankens', to: 'ankens#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170830151252) do
+ActiveRecord::Schema.define(version: 20170901005021) do
 
   create_table "ankens", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "customer_id",                   default: 0,  null: false
@@ -81,6 +81,20 @@ ActiveRecord::Schema.define(version: 20170830151252) do
     t.integer  "del_flg"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "title"
+    t.text     "detail",     limit: 65535
+    t.date     "due"
+    t.integer  "status"
+    t.text     "remark",     limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  create_table "test", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "groups"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
