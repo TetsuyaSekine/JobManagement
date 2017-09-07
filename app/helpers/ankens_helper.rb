5b6451cfa465746f59faaa21811b133bb771f855
tetsuya.sekine <gfbf5_tetsu@yahoo.co.jp>
2017-09-07 23:16:35 +0900

    ログ消し

--- app/helpers/ankens_helper.rb
+++ app/helpers/ankens_helper.rb
@@ -2,15 +2,9 @@ module AnkensHelper
 
   def chk_checked(array,value)
     if array.present?
-      logger.debug "ログだよ"
-      logger.debug array
-      logger.debug value
       if array.find{|n| n == value.to_s}.present?
-        logger.debug "trueなり"
         return true
       else
-        logger.debug "falseなり"
-
         return false
       end
     else
